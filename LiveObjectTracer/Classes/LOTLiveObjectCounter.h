8792211a21b93535e41479065d90f90bc5110b50
wagyu298 <wagyu298@gmail.com>
2017-03-18 14:32:51 +0900

    Doxygen added and unrequired prototypes removed.

--- LiveObjectTracer/Classes/LOTLiveObjectCounter.h
+++ LiveObjectTracer/Classes/LOTLiveObjectCounter.h
@@ -5,19 +5,45 @@
 
 @class LOTLiveObjectCounter;
 
+/*!
+ @protocol LOTLiveObjectCounterDelegate.
+ @brief Delegate protocol for LOTLiveObjectCounter
+ */
 @protocol LOTLiveObjectCounterDelegate <NSObject>
 
+/*!
+ @brief Called when LOTLiveObjectCounter's count property was changed.
+ @param counter Delegate method caller
+ @param count Live object count
+ @param previousCount Previous live object count
+ */
 - (void)lot_counter:(LOTLiveObjectCounter * _Nonnull)counter didChangeCount:(NSUInteger)count previousCount:(NSUInteger)previousCount;
 
 @end
 
+/*!
+ @class LOTLiveObjectCounter
+ @brief Manage number of live objects.
+ */
 @interface LOTLiveObjectCounter : NSObject
 
+/// @brief A delegate object.
 @property (nonatomic, nullable, weak) id <LOTLiveObjectCounterDelegate> delegate;
+
+/// @brief Number of live objects.
 @property (nonatomic, readonly) NSUInteger count;
 
+/*!
+ @brief Constructor with delegate object.
+ @param delegate A delegate object
+ @return Instance itself
+ */
 - (instancetype _Nonnull)initWithDelegate:(id <LOTLiveObjectCounterDelegate> _Nullable)delegate;
 
+/*!
+ @brief Add an object to count live objects.
+ @param object An object to manage
+ */
 - (void)addObject:(id _Nonnull)object;
 
 @end
--- LiveObjectTracer/Classes/LOTLiveObjectTracer.h
+++ LiveObjectTracer/Classes/LOTLiveObjectTracer.h
@@ -5,23 +5,46 @@
 
 @class LOTLiveObjectTracer;
 
+/*!
+ @protocol LOTLiveObjectTracerDelegate.
+ @brief Delegate protocol for LOTLiveObjectTracer
+ */
 @protocol LOTLiveObjectTracerDelegate <NSObject>
 
+/*!
+ @brief Called when LOTLiveObjectTracer tracing object was deallocated.
+ @param tracer Delegate method caller
+ */
 - (void)lot_tracerDidObjectReleased:(LOTLiveObjectTracer * _Nonnull)tracer;
 
 @end
 
+/*!
+ @class LOTLiveObjectTracer
+ @brief Trace the target object was live or dead.
+ */
 @interface LOTLiveObjectTracer : NSObject
 
+/// @brief A delegate object.
 @property (nonatomic, nullable, weak) id <LOTLiveObjectTracerDelegate> delegate;
 
+/// @brief YES if the object is live.
 @property (nonatomic, readonly) BOOL live;
+
+/// @brief YES if the object is dead (deallocated).
 @property (nonatomic, readonly) BOOL dead;
 
+/*!
+ @brief Constructor with tracing target object and delegate object.
+ @param object An object to trace living
+ @param delegate A delegate object
+ */
 - (instancetype _Nonnull)initWithObject:(id _Nonnull)object delegate:(id <LOTLiveObjectTracerDelegate> _Nullable)delegate;
-- (instancetype _Nonnull)initWithObject:(id _Nonnull)object;
 
-- (BOOL)live;
-- (BOOL)dead;
+/*!
+ @brief Constructor with tracing target object.
+ @param object An object to trace living
+ */
+- (instancetype _Nonnull)initWithObject:(id _Nonnull)object;
 
 @end
